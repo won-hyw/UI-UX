e088562daf48c10be05c6477872b8647110fe2f6
won-hyw <w2019@e-mirim.hs.kr>
2022-03-16 10:21:17 +0900

    [220316] 수업 내용 백업

--- c3_ex/playlist_ex/playlist/playlist.js
+++ c3_ex/playlist_ex/playlist/playlist.js
@@ -9,6 +9,19 @@ function init() {
 	loadPlaylist();
 }
 
-	//추가1
+function handleButtonClick(e) {
+	var textInput = document.getElementById("songTextInput");
+	var songName = textInput.value;
+	// alert("Adding " + songName);
 
+	if (songName == "") {
+		alert("곡을 입력하세요");
+	} else {
+		var li = document.createElement("li");
+		li.innerHTML = songName;
+		var ul = document.getElementById("playlist");
+		ul.append(li);
+		save(songName);
+	}
+}
 
